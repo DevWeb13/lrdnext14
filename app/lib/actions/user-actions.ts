14d06feba5c9b77e43ca2754874c4ea0c7986a12
DevWeb13 <giuliano.loic@sfr.fr>
2023-12-01 10:32:00 +0100

    feat: Add dependencies and update layout.tsx

---
+++ app/lib/actions/user-actions.ts
@@ -0,0 +1,8 @@
+// 'use server';
+
+// import { z } from 'zod';
+// import { sql } from '@vercel/postgres';
+// import { revalidatePath } from 'next/cache';
+// import { redirect } from 'next/navigation';
+// import { signIn } from '@/auth';
+// import bcrypt from 'bcrypt';
