fac604b372733a58f0d83c73104f1c18ca4cbf2a
DevWeb13 <giuliano.loic@sfr.fr>
2023-12-13 09:20:36 +0100

    fix: (auth.config.ts): Remove this commented out code. https://sonarcloud.io/project/issues?open=AYxeyGhCoC-AymsuqnCJ&id=DevWeb13_lrdnext14

--- auth.config.ts
+++ auth.config.ts
@@ -6,7 +6,6 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      // console.log({ auth, nextUrl });
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       const isOnLogout = nextUrl.pathname.startsWith('/auth/logout');
