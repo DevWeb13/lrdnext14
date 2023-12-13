5bf30ab292a2f46c2216114942e768b130e5a135
DevWeb13 <giuliano.loic@sfr.fr>
2023-12-13 09:10:34 +0100

    fix: (app/ui/dashboard/displayUserData.tsx): Remove this commented out code. app/ui/dashboard/displayUserData.tsx

--- app/ui/dashboard/displayUserData.tsx
+++ app/ui/dashboard/displayUserData.tsx
@@ -1,24 +1,17 @@
 import { AppUser } from '@/types/app-user';
 import { auth } from '@/auth';
-// import Image from 'next/image';
 
 export default async function DisplayUserData() {
   const session = await auth();
   console.log({ session });
   const user = session?.user as AppUser;
-  // console.log({ user });
-  const { email, name, image } = user;
+
+  const { email, name } = user;
   return (
     <div>
       <h1>Display User Data</h1>
       <p>{email}</p>
       <p>{name}</p>
-      {/* <Image
-        src={image}
-        alt={name}
-        width={200}
-        height={200}
-      /> */}
     </div>
   );
 }
